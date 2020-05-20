96281b1d4ad86b631d18628da2a1f35b39a97a28
Edouard Lacan <edouard.lacan@yoomap.fr>
2020-05-20 16:21:14 +0200

    port 80 gros

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Fuck The World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
 
